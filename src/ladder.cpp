7ae86c6374b891055d3d005d3f933830f8ee3db0
Chansik Yoon <chansiky@uci.edu>
2025-03-16 10:16:54 +0000

    edit_distance_within() function has been implemented.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -3,3 +3,25 @@
 void error(std::string word1, std::string word2, std::string msg) {
     std::cerr << "Error: " << msg << " (" << word1 << " -> " << word2 << ")" << std::endl;
 }
+
+bool edit_distance_within(const std::string & str1, const std::string & str2, int d) {
+    int len_str1 = str1.size(), len_str2 = str2.size();
+    if (abs(len_str1 - len_str2) > 1) {
+        return false;
+    }
+    int diff_count = 0;
+    for (size_t i = 0, j = 0; i < len_str1 && j < len_str2; i++, j++) {
+        if (str1[i] != str2[j]) {
+            diff_count++;
+            if (len_str1 > len_str2) {
+                j--;
+            } else if (len_str1 < len_str2) {
+                i--;
+            }
+        }
+        if (diff_count > d) {
+            return false;
+        }
+    }
+    return true;
+}
