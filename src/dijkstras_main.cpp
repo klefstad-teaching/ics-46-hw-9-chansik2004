b330e8d9375bccea61b054338ca2fdaa04c56efe
Chansik Yoon <chansiky@uci.edu>
2025-03-16 12:59:51 +0000

    main() function for dijkstras_main.cpp has been implemented.

---
+++ src/dijkstras_main.cpp
@@ -0,0 +1,19 @@
+#include "dijkstras.h"
+
+int main(int argc, char * argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+
+    Graph G;
+    file_to_graph(argv[1], G);
+
+    std::vector<int> previous;
+    std::vector<int> distance = dijkstra_shortest_path(G, 0, previous);
+
+    for (int i = 0; i < G.numVertices; ++i) {
+        std::vector<int> shortestPath = extract_shortest_path(previous, i);
+        print_path(shortestPath, distance[i]);
+    }
+}
