c559e971b9cd3e4c65ec41d8203fb82e3a97688a
Chansik Yoon <chansiky@uci.edu>
2025-03-16 11:01:10 +0000

    verify_word_ladder() function has been implemented.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -89,3 +89,14 @@ void print_word_ladder(const std::vector<std::string> & ladder) {
     }
     std::cout << std::endl;
 }
+
+void verify_word_ladder() {
+    std::set<std::string> word_list;
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
