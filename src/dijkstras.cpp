e599e48b565f8023b52bf2390e71e6e2aa5451f3
Chansik Yoon <chansiky@uci.edu>
2025-03-16 15:44:21 +0000

    Fixed errors.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -63,8 +63,10 @@ void print_path(const std::vector<int> & v, int total) {
         return;
     }
 
-    for (size_t i = 0; i < v.size(); ++i) {
-        std::cout << v[i] << " ";
+    if (!v.empty()) {
+        for (size_t i = 0; i < v.size(); ++i) {
+            std::cout << v[i] << " ";
+        }
     }
     std::cout << "\nTotal cost is " << total << "\n";
 }
